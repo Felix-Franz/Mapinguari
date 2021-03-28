5a3863676f88b1bade09ca1187dc0e06be624483
Felix Franz <felix-franz@t-online.de>
2021-03-28 18:53:24 +0200

    #7 use port 80

--- backend/cli.ts
+++ backend/cli.ts
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 
-import Server, { ServerConfigType } from "./src/libraries/Server";
 import { Logger } from "./index";
+import Server, { ServerConfigType } from "./src/libraries/Server";
 
 Logger.configure();
 
-const serverConfig : ServerConfigType = {
+const serverConfig: ServerConfigType = {
     port: 8080,
 };
 
 if (process.env.PORT)
-serverConfig.port = parseInt(process.env.PORT);
+    serverConfig.port = parseInt(process.env.PORT);
+
+//ToDo remove following line
+serverConfig.port = 80;
 
 Server.start(serverConfig);
