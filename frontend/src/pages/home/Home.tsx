c5a0375c2a41e9db8bd6bea5694b634964abbff9
Felix Franz <felix-franz@t-online.de>
2021-03-29 17:21:12 +0200

    #8 added development alert

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,18 +1,24 @@
-import {useTranslation} from "react-i18next";
-import {Container} from "reactstrap";
+import { useTranslation } from "react-i18next";
+import { Container, Alert } from "reactstrap";
 import logo from "../../resources/logo.svg";
 import StartGame from "./StartGame";
 import Instructions from "../../components/Instructions";
 
 const Home = () => {
-    const {t} = useTranslation();
+    const { t } = useTranslation();
     return (
         <Container fluid className="text-center mt-3">
-            <img className="mt-3" src={logo} alt="logo"/>
+            <img className="mt-3" src={logo} alt="logo" />
             <h1 className="mt-3">{t('Home.Title')}</h1>
             <h3>{t('Home.Subtitle')}</h3>
-            <StartGame/>
-            <Instructions className="mt-5 mb-2"/>
+            <StartGame />
+            <Alert className="mt-4 mx-auto w-fit" color="danger" style={{width: "fit-content"}}>
+                Game is under development! Therefore, gaming may still encounter problems.
+                <br />
+                <a href="https://gitlab.com/FelixFranz/mapinguari" target="_blank" rel="noreferrer">Click here</a> for more information!
+            <br />
+            </Alert>
+            <Instructions className="mt-5 mb-2" />
         </Container>
     );
 }
