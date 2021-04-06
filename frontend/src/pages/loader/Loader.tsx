c51bee8ba3e3295daffacda532a172c3c85aafb7
Felix Franz <felix-franz@t-online.de>
2021-04-06 18:28:21 +0000

    #18 added loading page

--- frontend/src/pages/loader/Loader.tsx
+++ frontend/src/pages/loader/Loader.tsx
@@ -1,6 +1,15 @@
-// loading component for suspense fallback
+import { Container, Spinner } from "reactstrap";
+import logo from "../../resources/logo.svg";
+
+// loading component for suspense fallback etc.
 const Loader = () => (
-        <div>ToDo loading...</div>
+    <Container fluid className="text-center my-3">
+        <img className="mt-3" src={logo} alt="logo" />
+        <div className="mt-5">
+            <Spinner color="primary" className="align-middle" />
+            <span className="ml-2 align-middle">Loading...</span>
+        </div>
+    </Container>
 );
 
 export default Loader;
