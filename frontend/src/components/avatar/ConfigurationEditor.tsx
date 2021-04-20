9ca2f022e363e0bb2d3fe1c5b5a287079d0f3c34
Felix Franz <felix-franz@t-online.de>
2021-04-20 15:03:16 +0000

    #37 changed avatar configuration

--- frontend/src/components/avatar/ConfigurationEditor.tsx
+++ frontend/src/components/avatar/ConfigurationEditor.tsx
@@ -1,6 +1,8 @@
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
-import { Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Form, FormGroup, Input, InputGroup, InputGroupAddon, Label } from "reactstrap";
 import AvatarConfigurationType from "../../core/types/AvatarConfigurationType";
 import { AvatarConfigurationOptions } from "./Avatar";
 
@@ -15,13 +17,30 @@ const ConfigurationEditor: FC<{ configuration: AvatarConfigurationType, onChange
     }
 
     const generateSelect = (key: string, hide: boolean = false) => {
+        // @ts-ignore
+        const currentConfig: string = configuration[key];
+        // @ts-ignore
+        const configOptions: string[] = AvatarConfigurationOptions[key];
+        const currentConfigIndex = configOptions.findIndex(s => s === currentConfig);
+        const prevConfig = configOptions[(currentConfigIndex - 1 + configOptions.length) % configOptions.length];
+        const nextConfig = configOptions[(currentConfigIndex + 1) % configOptions.length];
         return <FormGroup className={`mb-1 ${hide ? "d-none" : ""}`}>
             <Label for={key} className="mb-0">{t(`AvatarEditor.${key.replace(/^(.)(.*)$/, (a, b, c) => `${b.toUpperCase()}${c}`)}`)}</Label>
-            {/* @ts-ignore */}
-            <Input type="select" name={key} value={configuration[key]} onChange={e => change(key, e.target.value)}>
-                {/* @ts-ignore */}
-                {AvatarConfigurationOptions[key].map((o, i) => <option key={i}>{o}</option>)}
-            </Input>
+            <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                    <Button color="primary" outline onClick={() => change(key, prevConfig)}>
+                        <FontAwesomeIcon icon={faArrowLeft} />
+                        </Button>
+                </InputGroupAddon>
+                <Input type="select" name={key} value={currentConfig} onChange={e => change(key, e.target.value)}>
+                    {configOptions.map((o, i) => <option key={i}>{o}</option>)}
+                </Input>
+                <InputGroupAddon addonType="append">
+                    <Button color="primary" outline onClick={() => change(key, nextConfig)}>
+                        <FontAwesomeIcon icon={faArrowRight} />
+                        </Button>
+                </InputGroupAddon>
+            </InputGroup>
         </FormGroup >
     }
 
