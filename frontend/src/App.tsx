a9c27537d614a4ae67c4c7440bff7d1636a90ba4
Felix Franz <felix-franz@t-online.de>
2021-07-14 08:40:51 +0000

    #54 removed unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import { Suspense, useEffect, useState } from 'react';
+import LoadingModal from './components/LoadingModal';
 import ServiceClient from './libraries/ServiceClient';
 import SocketClient from "./libraries/SocketClient";
 import Loader from "./pages/loader/Loader";
 import Router from "./pages/Router";
-import { Modal, Spinner } from "reactstrap";
-import LoadingModal from './components/LoadingModal';
 
 // here app catches the suspense from page in case translations are not yet loaded
 export default function App() {
